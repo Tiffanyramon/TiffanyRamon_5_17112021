e81b92a62c2f9adb2e655581df8d0c50677d89c1
Tiffanyramon <86716897+Tiffanyramon@users.noreply.github.com>
2022-01-10 14:06:10 +0100

    commit 7

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,24 +1,26 @@
 //Récupération des élements du panier.
-let productLocalStorage = JSON.parse(localStorage.getItem("produit"));
-console.table(productLocalStorage);
+let articles = JSON.parse(localStorage.getItem("panier"));
+console.table(articles);
 
 //récipération des donnée de l'API.
-then (function (){
+let totalPrice = 0;
+let totalQuantity = 0; 
     articles.forEach(element => {
-        let items = document.getElementById(' cart_items');
-items.innerHTML += `<a href="./js/cart.js data-id=${element.idProduct}" data-color="${element.choiceColor}">             <div class="cart__item__img">
-                  <img src="${element.article.imageUrl}" alt="${element.article.altTxt}">
+        let items = document.getElementById('cart__items');
+items.innerHTML += `<article class="cart__item" data-id="${element.idProduct}" data-color="${element.choiceColors}">            
+                  <div class="cart__item__img">
+                  <img src="${element.productImgSrc}" alt="${element.productImgAlt}">
                 </div>
                 <div class="cart__item__content">
-                  <div class="${element.article.description}">
-                    <h2>class="productName">${element.name}</h2>
-                    <p>class="color">${element.choiceColor}</p>
-                    <p>class="price">${element.article.price}</p>
+                  <div class="${element.productDescription}">
+                    <h2 class="productName">${element.productName}</h2>
+                    <p class="color">${element.choiceColors}</p>
+                    <p class="price">${element.productPrice}</p>
                   </div>
                   <div class="cart__item__content__settings">
-                    <div class="${element.choiceQuantity}">
+                    <div class="cart__item__content__settings__quantity">
                       <p>Qté : </p>
-                      <input type="number" class="${element.choiceQuantity}" name="itemQuantity" min="1" max="100" value="42">
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${element.choiceQuantity}">
                     </div>
                     <div class="cart__item__content__settings__delete">
                       <p class="deleteItem">Supprimer</p>
@@ -26,18 +28,29 @@ items.innerHTML += `<a href="./js/cart.js data-id=${element.idProduct}" data-col
                   </div>
                 </div>
               </article>`
-        
+            totalPrice += element.productPrice * element.choiceQuantity
+            totalQuantity += element.choiceQuantity
+   
     });
- })
+ const  total = document.getElementById("totalPrice");
+ total.innerHTML = totalPrice
 
- //Calcul du prix total par produit.
+ const quantite = document.getElementById("totalQuantity");
+ quantite.innerHTML = totalQuantity
 
- const inputQantity = document.querySelectorAll("choiceQuantity");
+ //Changer la quantité.
+
+ const inputQantity = document.querySelectorAll(".itemQuantity");
 
  inputQantity.forEach(function(input){
      input.addEventListener('change',function(event){
-         const idProduct = input.closest('article').dataset.idProduct
-         const choiceColors = input.closest('article').dataset.colorColors
+         const idProduct = input.closest('article').dataset.id
+         const choiceColors = input.closest('article').dataset.color
+         
+         const products = articles.map((element)=> element.idProduct === idProduct && element.choiceColors === choiceColors ? { ...element,choiceQuantity:parseInt(event.target.value)} : element)
+         localStorage.setItem("panier",JSON.stringify(products))
+         location.reload()
+         
      })
  })
 
@@ -67,5 +80,5 @@ items.innerHTML += `<a href="./js/cart.js data-id=${element.idProduct}" data-col
  }
 //Mettre à jour le localstorage avec la nouvelle quantité. 
 
-//window.location.relard() => rafraîchir la page.
-location.roload();
+//window.location.reload() => rafraîchir la page.
+//location.reload();
